b8b8f80cf48714c1b52690167acc8607b7310be8
George Zhang <georgezhang02@hotmail.com>
2022-01-05 00:50:18 -0800

    Miner going to first lead (WIP)

--- battlecode22-scaffold-master/src/dontatme/Archon.java
+++ battlecode22-scaffold-master/src/dontatme/Archon.java
@@ -3,24 +3,55 @@ package dontatme;
 import battlecode.common.*;
 
 public class Archon {
+
+    static int archonIndex;
+
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    public Archon() {
+        archonIndex = -1;
+    }
+
     /**
      * Run a single turn for an Archon.
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
     static void run(RobotController rc) throws GameActionException {
-        // Pick a direction to build in.
-        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
-        if (RobotPlayer.rng.nextBoolean()) {
-            // Let's try to build a miner.
-            rc.setIndicatorString("Trying to build a miner");
+
+        // Set the index where the archon will write the lead location
+        if (archonIndex == -1) {
+            archonIndex = (rc.getID() - 2) / 2;
+        }
+
+        // Save the first available lead location
+        if (rc.readSharedArray(archonIndex) == 0) {
+            MapLocation me = rc.getLocation();
+            for (int dx = -5; dx <= 5; dx++) {
+                for (int dy = -5; dy <= 5; dy++) {
+                    MapLocation leadLocation = new MapLocation(me.x + dx, me.y + dy);
+                    if (rc.canSenseLocation(leadLocation) && rc.senseLead(leadLocation) > 0) {
+                        rc.writeSharedArray(archonIndex, leadLocation.x * 64 + leadLocation.y);
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Build a miner in any direction
+        for (Direction dir : directions) {
             if (rc.canBuildRobot(RobotType.MINER, dir)) {
                 rc.buildRobot(RobotType.MINER, dir);
-            }
-        } else {
-            // Let's try to build a soldier.
-            rc.setIndicatorString("Trying to build a soldier");
-            if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
-                rc.buildRobot(RobotType.SOLDIER, dir);
+                rc.setIndicatorString("built miner");
+                break;
             }
         }
     }
--- battlecode22-scaffold-master/src/dontatme/Miner.java
+++ battlecode22-scaffold-master/src/dontatme/Miner.java
@@ -3,32 +3,82 @@ package dontatme;
 import battlecode.common.*;
 
 public class Miner {
+
+    static int archonIndex;
+    static int minerPhase;
+
+    public Miner() {
+        archonIndex = -1;
+        minerPhase = 0;
+    }
+
     /**
      * Run a single turn for a Miner.
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
     static void run(RobotController rc) throws GameActionException {
-        // Try to mine on squares around us.
+
         MapLocation me = rc.getLocation();
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dy = -1; dy <= 1; dy++) {
-                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
-                // Notice that the Miner's action cooldown is very low.
-                // You can mine multiple times per turn!
-                while (rc.canMineGold(mineLocation)) {
-                    rc.mineGold(mineLocation);
-                }
-                while (rc.canMineLead(mineLocation)) {
-                    rc.mineLead(mineLocation);
+        int leadLocation = rc.readSharedArray(archonIndex);
+        int leadX = leadLocation / 64;
+        int leadY = leadLocation % 64;
+
+        // Save the index of the archon it spawned from
+        if (archonIndex == -1) {
+            for (RobotInfo info : rc.senseNearbyRobots()) {
+                if (info.getType() == RobotType.ARCHON) {
+                    archonIndex = (info.getID() - 2) / 2;
                 }
             }
         }
 
-        // Also try to move randomly.
-        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
-        if (rc.canMove(dir)) {
-            rc.move(dir);
-            System.out.println("I moved!");
+        // If arrived, go toward mining phase
+        if (minerPhase == 0 && me.x == leadX && me.y == leadY) {
+            minerPhase = 1;
         }
-    } 
+
+        int mineCount = 0;
+
+        // Phase 0: navigate towards starting lead
+        if (minerPhase == 0) {
+            Direction dir = navigateTowards(leadX, leadY);
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+            }
+        }
+
+        // Phase 1: mine around
+        else if (minerPhase == 1){
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                    // Notice that the Miner's action cooldown is very low.
+                    // You can mine multiple times per turn!
+                    while (rc.canMineGold(mineLocation)) {
+                        rc.mineGold(mineLocation);
+                    }
+                    while (rc.canMineLead(mineLocation)) {
+                        mineCount++;
+                        rc.mineLead(mineLocation);
+                    }
+                }
+            }
+        }
+
+        // Phase 2: finish mining everything in vision
+        else if (minerPhase == 2) {
+
+        }
+
+        // If everything is mined, look for more stuff to mine
+        if (minerPhase == 1 && mineCount == 0) {
+            minerPhase = 2;
+        }
+
+    }
+
+    // Given a coordinate to go towards, return a direction to travel
+    static Direction navigateTowards(int x, int y) {
+        return Direction.CENTER;
+    }
 }
