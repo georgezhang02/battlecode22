f7ef5a61c00e1b1e8466dda2197523ea7ffe9f1e
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-01-10 17:50:06 -0500

    Watchtower attack code

--- battlecode22-scaffold-master/src/dontatme/Watchtower.java
+++ battlecode22-scaffold-master/src/dontatme/Watchtower.java
@@ -8,6 +8,14 @@ public strictfp class Watchtower {
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
     public static void run(RobotController rc) throws GameActionException {
-
+        int radius = rc.getType().actionRadiusSquared;
+        Team opponent = rc.getTeam().opponent();
+        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        if (enemies.length > 0) {
+            MapLocation toAttack = enemies[0].location;
+            if (rc.canAttack(toAttack)) {
+                rc.attack(toAttack);
+            }
+        }
     }
 }
