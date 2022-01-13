3a0538cbc82a3d3f4ae9b688e72abf5d7a6ee748
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-01-13 18:32:54 -0500

    Update Watchtower
    Cleaned the code and added comments

--- battlecode22-scaffold-master/src/dontatme/Watchtower.java
+++ battlecode22-scaffold-master/src/dontatme/Watchtower.java
@@ -9,12 +9,16 @@ public strictfp class Watchtower {
      * Run a single turn for a Watchtower.
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
+
+    //number of droids killed by watchtower
     static int killCount = 0;
 
     public static void run(RobotController rc) throws GameActionException {
         int radius = rc.getType().actionRadiusSquared;
         Team opponent = rc.getTeam().opponent();
         RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+
+        //attack
         int counter = 0;
         while (rc.isActionReady() && enemies.length > 0) {
             MapLocation toAttack = enemies[counter].location;
@@ -27,14 +31,13 @@ public strictfp class Watchtower {
                 killCount++;
                 counter++;
             }
+            //don't increment counter if the enemy is not killed -> try and attack them again
         }
     }
     static boolean checkAttackKilled(RobotController rc, RobotInfo enemy)
             throws GameActionException {
-        boolean killed = false;
-        if (rc.getType().getDamage(rc.getLevel()) > enemy.getHealth()) {
-            killed = true;
-        }
-        return killed;
+
+        //does the damage of my attack kill the enemy I targeted
+        return rc.getType().getDamage(rc.getLevel()) > enemy.getHealth();
     }
 }
