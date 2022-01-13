2f242c8f83854518be6c40bbc98b82f52c89f7f4
henry <s-hliao@lwsd.org>
2022-01-13 14:48:37 -0500

    Soldiers data

--- battlecode22-scaffold-master/src/dontatme/Soldier.java
+++ battlecode22-scaffold-master/src/dontatme/Soldier.java
@@ -3,6 +3,7 @@ package dontatme;
 import battlecode.common.*;
 
 import java.awt.*;
+import java.util.Map;
 
 public strictfp class Soldier {
     /**
@@ -18,6 +19,8 @@ public strictfp class Soldier {
     static MapLocation curTarget;
 
     static RobotController rc;
+
+    static int commandIteration;
 
     public Soldier (RobotController rc) throws GameActionException {
         this.rc = rc;
@@ -39,6 +42,9 @@ public strictfp class Soldier {
             case 2: //Exploring
                 explore();
                 break;
+            case 3:
+                prepForAnomaly(AnomalyType.ABYSS);
+                break;
 
         }
 
@@ -54,30 +60,50 @@ public strictfp class Soldier {
 
     }
 
-    static void baseAction(){
+
+    static void prepForAnomaly(AnomalyType anomalyType)
+    {
 
     }
+    static void offense(MapLocation target, int attackType) throws GameActionException {
+        if(!pathfinder.targetWithinRadius(target, 2)){
+            move(pathfinder.pathToTarget(target, false));
+            attack(attackType);
 
-    static void offense(MapLocation target, int attackType){
+        }  else{
+            MapLocation ml = attack(attackType);
+            if(ml!= null){
+                curTarget = ml;
+            } else{
+                state = 2;
+            }
+        }
+
 
     }
 
     static void defense(MapLocation target) throws GameActionException {
         if(!pathfinder.targetWithinRadius(target, 2)){
             move(pathfinder.pathToTarget(target, false));
+        } else{
+            MapLocation ml = attack(1);
+            if(ml== null){
+                state = 2;
+            }
         }
-        attack(1);
     }
 
+
     static void explore() throws GameActionException {
+        curTarget = null;
         move(pathfinder.pathToExplore());
         attack(1);
     }
 
-    static void attack(int attackType) throws GameActionException {
+    static MapLocation attack(int attackType) throws GameActionException {
 
         if(rc.isActionReady()){
-            MapLocation attackLoc;
+            RobotInfo attackLoc;
             if(attackType == 0){ // attack all
                 attackLoc = getAttack(3, 1, 0, 2);
             } else if(attackType == 1){ //attack droids
@@ -91,13 +117,15 @@ public strictfp class Soldier {
             }
 
             if(attackLoc != null){
-                rc.attack(attackLoc);
+                rc.attack(attackLoc.getLocation());
+                return attackLoc.getLocation();
             }
         }
+        return null;
     }
 
-    static MapLocation getAttack(int prio1, int prio2, int prio3, int prio4){
-        MapLocation[] ml = new MapLocation[4];
+    static RobotInfo getAttack(int prio1, int prio2, int prio3, int prio4){
+        RobotInfo[] ml = new RobotInfo[4];
         int[] minHealth = new int[4];
 
         for(RobotInfo robot : sensedInfo){
@@ -106,24 +134,24 @@ public strictfp class Soldier {
             if(type.isBuilding()){
                 if(type.equals(RobotType.ARCHON)){
                     if(id < minHealth[0]){
-                        ml[0] = robot.getLocation();
+                        ml[0] = robot;
                         minHealth[0] = id;
                     }
                 } else{
                     if(id < minHealth[1]){
-                        ml[1] = robot.getLocation();
+                        ml[1] = robot;
                         minHealth[1] = id;
                     }
                 }
             } else{
                 if(type.equals(RobotType.MINER)){
                     if(id < minHealth[2]){
-                        ml[2] = robot.getLocation();
+                        ml[2] = robot;
                         minHealth[2] = id;
                     }
                 } else {
                     if(id < minHealth[3]){
-                        ml[3] = robot.getLocation();
+                        ml[3] = robot;
                         minHealth[3] = id;
                     }
                 }
