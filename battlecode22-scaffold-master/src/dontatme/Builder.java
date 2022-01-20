265bc186b6880e1500237e4a6446622d1cedc196
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-01-19 22:35:13 -0500

    Builder Commands
    -

--- battlecode22-scaffold-master/src/dontatme/Builder.java
+++ battlecode22-scaffold-master/src/dontatme/Builder.java
@@ -14,15 +14,10 @@ public strictfp class Builder {
     static Pathfinder pathfinder;
     public static void run(RobotController rc) throws GameActionException {
         //part 1: see if there are any nearby buildings that are in prototype mode
-        searchNonMoveActions(rc);
 
-        //now just explore
-        Direction dir = pathfinder.pathToExplore();
-        while (rc.canMove(dir)) {
-            rc.move(dir);
-            searchNonMoveActions(rc);
-        }
     }
+    
+    //blow up for lead resources
     static void detonate(RobotController rc) throws GameActionException {
         //search areas near me in cardinal directions
         int lowestRubble = Integer.MAX_VALUE;
@@ -39,147 +34,153 @@ public strictfp class Builder {
         rc.disintegrate();
 
     }
-    static void searchNonMoveActions(RobotController rc) throws GameActionException {
-        RobotInfo [] buildings = getNearbyTowers(rc);
-        RobotInfo [] protoBuildings = findPrototypeTowers(rc, buildings);
 
-        //that means that there are prototype buildings, and see if we can prototype
-        if (protoBuildings != null) {
-            int counter = 0;
-            repairBuild(rc, protoBuildings);
-        }
-        //check if there's a level 1 building that can be mutated
-        if (buildings[0] != null ) {
-            seeBuildingMutation(rc, buildings);
-        }
-        //means that there is a nearby building
-        //check if it can be repaired
-        if (buildings[0] != null) {
-            repairBuild(rc, buildings);
-        }
-    }
-    static void seeBuildingMutation(RobotController rc, RobotInfo [] buildings) throws GameActionException {
-        int counter = 0;
-        while (counter < buildings.length && buildings[counter] != null ) {
-            if (buildings[counter].getLevel() == 1 && buildings[counter].getType().equals(RobotType.WATCHTOWER)) {
-                if (rc.canMutate(buildings[counter].getLocation()))//also implement watchtower analysis
-                    rc.mutate(buildings[counter].getLocation());
-            }
-            counter++;
-        }
-    }
-    static void repairBuild(RobotController rc, RobotInfo [] buildings) throws GameActionException{
-        int counter = 0;
-        while (counter < buildings.length && buildings[counter] != null) {
-            if (rc.canRepair(buildings[counter].getLocation())) {
-                rc.repair(buildings[counter].getLocation());
-            }
-            counter++;
-        }
-    }
-
-    static void advance(RobotController rc) throws GameActionException {
-        //nub -> in development
-        MapLocation nearestArchon = findNearestArchon(rc);
-        int activationNumber = 4; //number of nearby enemies to trigger tower building
-        int numberNearbyEnemies = getNumberOfNearbyEnemies(rc);
-        if (rc.isActionReady() && numberNearbyEnemies >= activationNumber) {
-            Direction dir = findPlaceToBuildTower(rc);
-            if (dir != null)
-                rc.buildRobot(RobotType.WATCHTOWER, dir);
-            else {
-                //fill in with path planning code
-                //if it can't build a tower, it needs to continue advancing
-            }
-        }
-
-        else {
-           //fill in with path planning code
-        }
-    }
-
-    static Direction findPlaceToBuildTower(RobotController rc) throws GameActionException {
-        Direction dir = Helper.directions[0];
-        int counter = 1;
-        boolean canBuildTower = rc.canBuildRobot(RobotType.WATCHTOWER, dir);
-        int lowestRubble = Integer.MAX_VALUE;
-        int bestIndex = Integer.MAX_VALUE;
-
-        while (counter < Helper.directions.length) {
-            dir = Helper.directions[counter];
-            //find the rubble count on all squares that a watchtower can be built
-            if (rc.canBuildRobot(RobotType.WATCHTOWER, dir)) {
-                //finds new position
-                MapLocation location = new MapLocation(rc.getLocation().x +
-                        Helper.directions[counter].dx, rc.getLocation().y +
-                        Helper.directions[counter].dy);
-                int rubbleCount = rc.senseRubble(location);
-                if (rubbleCount < lowestRubble) {
-                    lowestRubble = rubbleCount;
-                    bestIndex = counter;
-                }
-            }
-            counter++;
-        }
-
-        //if there were no towers that could be built
-        if (bestIndex == Integer.MAX_VALUE) {
-            dir = null;
-        }
-        else {
-            dir = Helper.directions[bestIndex];
-        }
-        return dir;
-    }
-
-    static MapLocation findNearestArchon(RobotController rc) throws GameActionException {
-        MapLocation nearestArchon = new MapLocation(0, 0); //placeholder
-        return nearestArchon;
-    }
-    static int getNumberOfNearbyEnemies(RobotController rc) throws GameActionException {
-        //find number of nearby enemy opponents
-        Team opponent = rc.getTeam().opponent();
-        int radius = rc.getType().actionRadiusSquared;
-        RobotInfo [] enemyBots = rc.senseNearbyRobots(radius, opponent);
-        return enemyBots.length;
-    }
-    static RobotInfo [] getAllies(RobotController rc) throws GameActionException {
-        Team ally = rc.getTeam();
-        int radius = rc.getType().actionRadiusSquared;
-        return rc.senseNearbyRobots(radius, ally);
-    }
-    static RobotInfo [] getNearbyTowers(RobotController rc) throws GameActionException {
-        RobotInfo [] allies = getAllies(rc);
-
-        //an ArrayList could be used here, but I believe it will use much more Bytecode
-        RobotInfo [] buildings = new RobotInfo [allies.length];
-        int buildingCounter = 0;
-
-        for (int i = 0; i < allies.length; i++) {
-            if (allies[i].getType().isBuilding()) {
-                buildings[buildingCounter] = allies[i];
-                buildingCounter++;
-            }
-        }
-        return buildings;
-    }
-    static RobotInfo [] findPrototypeTowers(RobotController rc, RobotInfo [] buildings)
-            throws GameActionException {
-
-        RobotInfo [] protoBuildings = new RobotInfo[buildings.length];
-        if (buildings == null)
+    static RobotInfo [] findNearbyProtoTowers(RobotController rc) {
+        RobotInfo [] nearbyProtos = findNearbyProtos(rc);
+        if (nearbyProtos[0] == null)
             return null;
-        else {
-            int counter = 0;
-            int protoCounter = 0;
-            while (buildings[counter] != null && counter < buildings.length) {
-                if (buildings[counter].getMode() == RobotMode.PROTOTYPE) {
-                    protoBuildings[protoCounter] = buildings[counter];
-                    protoCounter++;
-                }
-                counter++;
+
+        RobotInfo [] nearbyProtoTowers = findNearbyTowers(rc, nearbyProtos);
+
+        if (nearbyProtoTowers[0] == null)
+            return null;
+
+        return nearbyProtoTowers;
+    }
+
+    static RobotInfo [] findNearbyProtoLabs(RobotController rc) {
+        RobotInfo [] nearbyProtos = findNearbyProtos(rc);
+        if (nearbyProtos[0] == null)
+            return null;
+
+        RobotInfo [] nearbyProtoLabs = findNearbyLabs(rc, nearbyProtos);
+
+        if (nearbyProtoLabs[0] == null)
+            return null;
+
+        return nearbyProtoLabs;
+    }
+
+    //find nearby prototypes
+    static RobotInfo [] findNearbyProtos(RobotController rc) {
+        RobotInfo [] nearbyBuildings = findNearbyBuildings(rc);
+        if (nearbyBuildings[0] == null)
+            return null;
+
+        RobotInfo [] nearbyProtos = new RobotInfo[nearbyBuildings.length];
+        int numberProtos = 0;
+
+        for (int i = 0; i < nearbyBuildings.length; i++) {
+            if (nearbyBuildings[i].getMode().equals(RobotMode.PROTOTYPE)) {
+                nearbyProtos[numberProtos] = nearbyBuildings[i];
+                numberProtos++;
             }
         }
-        return protoBuildings;
+        if (nearbyProtos[0] == null)
+            return null;
+
+        return nearbyProtos;
+    }
+    
+    //get nearby towers
+    static RobotInfo [] findNearbyTowers(RobotController rc) {
+        RobotInfo [] nearbyBuildings = findNearbyBuildings(rc);
+        if (nearbyBuildings[0] == null)
+            return null;
+
+        RobotInfo [] nearbyTowers = new RobotInfo [nearbyBuildings.length];
+        int numberTowers = 0;
+
+        for (int i = 0; i < nearbyBuildings.length; i++) {
+            if (nearbyBuildings[i].getType().equals(RobotType.WATCHTOWER)) {
+                nearbyTowers[numberTowers] = nearbyBuildings[i];
+                numberTowers++;
+            }
+        }
+        
+        if (nearbyTowers[0] == null)
+            return null;
+        return nearbyTowers;
+        
+    }
+    static RobotInfo [] findNearbyTowers(RobotController rc, RobotInfo [] nearbyBuildings) {
+        if (nearbyBuildings[0] == null)
+            return null;
+
+        RobotInfo [] nearbyTowers = new RobotInfo [nearbyBuildings.length];
+        int numberTowers = 0;
+
+        for (int i = 0; i < nearbyBuildings.length; i++) {
+            if (nearbyBuildings[i].getType().equals(RobotType.WATCHTOWER)) {
+                nearbyTowers[numberTowers] = nearbyBuildings[i];
+                numberTowers++;
+            }
+        }
+
+        if (nearbyTowers[0] == null)
+            return null;
+        return nearbyTowers;
+
+    }
+    
+    //get nearby labs
+    static RobotInfo [] findNearbyLabs(RobotController rc) {
+        RobotInfo [] nearbyBuildings = findNearbyBuildings(rc);
+        if (nearbyBuildings[0] == null)
+            return null;
+
+        RobotInfo [] nearbyLabs = new RobotInfo [nearbyBuildings.length];
+        int numberLabs = 0;
+
+        for (int i = 0; i < nearbyBuildings.length; i++) {
+            if (nearbyBuildings[i].getType().equals(RobotType.LABORATORY)) {
+                nearbyLabs[numberLabs] = nearbyBuildings[i];
+                numberLabs++;
+            }
+        }
+
+        if (nearbyLabs[0] == null)
+            return null;
+        return nearbyLabs;
+    }
+
+    static RobotInfo [] findNearbyLabs(RobotController rc, RobotInfo [] nearbyBuildings) {
+        if (nearbyBuildings[0] == null)
+            return null;
+
+        RobotInfo [] nearbyLabs = new RobotInfo [nearbyBuildings.length];
+        int numberLabs = 0;
+
+        for (int i = 0; i < nearbyBuildings.length; i++) {
+            if (nearbyBuildings[i].getType().equals(RobotType.LABORATORY)) {
+                nearbyLabs[numberLabs] = nearbyBuildings[i];
+                numberLabs++;
+            }
+        }
+
+        if (nearbyLabs[0] == null)
+            return null;
+        return nearbyLabs;
+    }
+
+    //get nearby buildings
+    static RobotInfo [] findNearbyBuildings(RobotController rc) {
+        Team myTeam = rc.getTeam();
+        RobotInfo [] nearbyRobots = rc.senseNearbyRobots(rc.getType().visionRadiusSquared,
+                myTeam);
+        RobotInfo [] nearbyBuildings = new RobotInfo [10];
+        int numberBuildingsFound = 0;
+        for (int i = 0; i < nearbyRobots.length; i++) {
+            if (nearbyRobots[i].getType().isBuilding()) {
+                nearbyBuildings[numberBuildingsFound] = nearbyRobots[i];
+                numberBuildingsFound++;
+            }
+        }
+
+        //handle null values aka no buildings found
+        if (numberBuildingsFound == 0)
+            return null;
+        else
+            return nearbyBuildings;
     }
 }
