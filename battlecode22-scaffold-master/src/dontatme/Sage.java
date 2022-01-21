41d359b3d42741b3ca7c99b15b6ccae6a74c5df4
Jason Zhang <zjjc123@hotmail.com>
2022-01-21 02:15:31 -0800

    Sage Extend Soldier

--- battlecode22-scaffold-master/src/dontatme/Sage.java
+++ battlecode22-scaffold-master/src/dontatme/Sage.java
@@ -2,12 +2,36 @@ package dontatme;
 
 import battlecode.common.*;
 
-public strictfp class Sage {
+public strictfp class Sage extends Soldier {
+
+    public static final int CHARGE_THRESHOLD = 2;
+    public static final int SAGE_RANGE = 25;
+
+    public Sage(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
     /**
      * Run a single turn for a Sage.
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
-    public static void run(RobotController rc) throws GameActionException {
+    public void run(RobotController rc) throws GameActionException {
+        int enemyCount = 0;
 
+        Team opponent = rc.getTeam().opponent();
+        enemies = rc.senseNearbyRobots(SAGE_RANGE, opponent);
+
+        for(RobotInfo robot:enemies){
+            if(robot.getType() == RobotType.SOLDIER || robot.getType() == RobotType.WATCHTOWER){
+                enemyCount++;
+            }
+        }
+
+        if (enemyCount > CHARGE_THRESHOLD && rc.canEnvision(AnomalyType.CHARGE)) {
+            rc.envision(AnomalyType.CHARGE);
+        }
+        
+        // run soldier class
+        super.run();
     }
 }
