a7c291d7302f45950dce513894800e755155b445
Karthik Shaji <58968966+HoodedPitohui@users.noreply.github.com>
2022-01-21 21:30:25 -0500

    lab update

--- battlecode22-scaffold-master/src/dontatme/Laboratory.java
+++ battlecode22-scaffold-master/src/dontatme/Laboratory.java
@@ -51,25 +51,83 @@ public strictfp class Laboratory {
             MapLocation loc23 = loc8.add(Helper.directions[7]);
             MapLocation loc24 = loc8.add(Helper.directions[0]);
 
+            MapLocation loc25 = loc9.add(Helper.directions[0]);
+            MapLocation loc26 = loc10.add(Helper.directions[0]);
+            MapLocation loc27 = loc11.add(Helper.directions[0]);
+            MapLocation loc28 = loc11.add(Helper.directions[1]);
+            MapLocation loc29 = loc11.add(Helper.directions[2]);
+            MapLocation loc30 = loc12.add(Helper.directions[2]);
+            MapLocation loc31 = loc13.add(Helper.directions[2]);
+            MapLocation loc32 = loc14.add(Helper.directions[2]);
+            MapLocation loc33 = loc15.add(Helper.directions[2]);
+            MapLocation loc34 = loc15.add(Helper.directions[3]);
+            MapLocation loc35 = loc15.add(Helper.directions[4]);
+            MapLocation loc36 = loc16.add(Helper.directions[4]);
+            MapLocation loc37 = loc17.add(Helper.directions[4]);
+            MapLocation loc38 = loc18.add(Helper.directions[4]);
+            MapLocation loc39 = loc19.add(Helper.directions[4]);
+            MapLocation loc40 = loc19.add(Helper.directions[5]);
+            MapLocation loc41 = loc19.add(Helper.directions[6]);
+            MapLocation loc42 = loc20.add(Helper.directions[6]);
+            MapLocation loc43 = loc21.add(Helper.directions[6]);
+            MapLocation loc44 = loc22.add(Helper.directions[6]);
+            MapLocation loc45 = loc23.add(Helper.directions[6]);
+            MapLocation loc46 = loc23.add(Helper.directions[7]);
+            MapLocation loc47 = loc23.add(Helper.directions[0]);
+            MapLocation loc48 = loc24.add(Helper.directions[0]);
 
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[0]),
-                    bestCoord);
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[1]),
-                    bestCoord);
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[2]),
-                    bestCoord);
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[3]),
-                    bestCoord);
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[4]),
-                    bestCoord);
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[5]),
-                    bestCoord);
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[6]),
-                    bestCoord);
-            bestCoord = checkIfLowRubble(rc, rc.getLocation().add(Helper.directions[7]),
-                    bestCoord);
+            //produces a sufficiently large search space
 
-            bestCoord = checkIfLowRubble(rc, rc.getLocation() ;
+            bestCoord = checkIfLowRubble(rc, loc1, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc2, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc3, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc4, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc5, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc6, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc7, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc8, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc9, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc10, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc11, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc12, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc13, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc14, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc15, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc16, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc17, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc18, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc19, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc20, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc21, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc22, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc23, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc24, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc25, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc26, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc27, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc28, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc29, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc30, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc31, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc32, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc33, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc34, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc35, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc36, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc37, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc38, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc39, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc40, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc41, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc42, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc43, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc44, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc45, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc46, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc47, bestCoord);
+            bestCoord = checkIfLowRubble(rc, loc48, bestCoord);
+
+
 
 
             if (rc.senseRubble(bestCoord) <= rc.senseRubble(rc.getLocation()) - 10) {
@@ -82,7 +140,8 @@ public strictfp class Laboratory {
     }
 
     static MapLocation checkIfLowRubble(RobotController rc, MapLocation loc, MapLocation bestLoc) throws GameActionException {
-        if (rc.senseRubble(loc) < rc.senseRubble(bestLoc)) {
+        if (rc.senseRubble(loc) < rc.senseRubble(bestLoc) &&
+            !rc.canSenseRobotAtLocation(loc)) {
             return loc;
         }
         else {
